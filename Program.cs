11e1bd871f98a3026f4e99d6a0f6a9ddd5aff60d
nickpinecone <nick.pineconefir@gmail.com>
2025-06-06 17:03:15 +0500

    Clean up

--- Program.cs
+++ Program.cs
@@ -1,49 +1,8 @@
-﻿using System.Threading.Tasks;
-using Terminal.Gui;
-
-namespace Rustle;
+﻿namespace Rustle;
 
 public static class Program
 {
-    private static Label _trackName = null!;
-    private static Mpv.Player _player = null!;
-
-    private static async Task InitializePlayer()
+    public static void Main()
     {
-        await _player.Initialize();
-
-        _player.MediaTitleChange += (sender, title) =>
-        {
-            Application.MainLoop.Invoke(() => { _trackName.Text = title; });
-        };
-
-        await _player.PlayAsync("http://rdstream-0625.dez.ovh:8000/radio.mp3");
-    }
-
-    public static async Task Main()
-    {
-        Application.Init();
-        
-        Application.Top.ColorScheme = new ColorScheme()
-        {
-            Normal = Application.Driver.MakeAttribute(Color.Black, Color.Black),
-        };
-
-        _trackName = new Label("Rustle")
-        {
-            X = Pos.Center(),
-            Y = Pos.Center(),
-            Height = 1,
-        };
-        
-        Application.Top.Add(_trackName);
-
-        _player = new Mpv.Player();
-        _ = InitializePlayer();
-
-        Application.Run();
-        Application.Shutdown();
-
-        await _player.DisposeAsync();
     }
 }
